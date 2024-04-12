1de90e355355ded69c850cca9a5a6d727ec7d6c9
Viktor Filippov <jackal3166@gmail.com>
2024-04-12 12:31:53 +0300

    Add map

--- app/(tabs)/map.jsx
+++ app/(tabs)/map.jsx
@@ -1,9 +1,35 @@
-import { Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { Dimensions } from 'react-native';
+import { restaurants } from '../../data'
 
-export default function Tab() {
-  return (
-    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-      <Text>Tab [Map]</Text>
-    </View>
-  );
-}
+const Screen = Dimensions.get('window');
+
+const ASPECT_RATIO = Screen.width / Screen.height;
+const LATITUDE_DELTA = 6;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const INITIAL_POSITION = {
+	latitude: 28.1305,
+	longitude: -81.4497,
+	latitudeDelta: LATITUDE_DELTA,
+	longitudeDelta: LONGITUDE_DELTA,
+};
+
+const MapScreen = () => {
+	return (
+		<MapView className="flex-1" initialRegion={INITIAL_POSITION} liteMode>
+			{restaurants.map((restaurant) => (
+				<Marker
+					key={restaurant.id}
+					coordinate={{
+						latitude: restaurant.latitude,
+						longitude: restaurant.longitude,
+					}}
+					title={restaurant.name}
+				/>
+			))}
+		</MapView>
+	);
+};
+
+export default MapScreen;
